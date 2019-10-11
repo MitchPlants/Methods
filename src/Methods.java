7cb3ea6b923b179c04936f6a4ca1a3a12d84fe9a
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-10-11 10:00:45 -0400

    Hello name + multiply numbers together

--- src/Methods.java
+++ src/Methods.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Methods {
     public static void main(String[]args){
+        String Name;
+        Scanner Keyboard;
+        Keyboard = new Scanner(System.in);
         helloWorld();
-        
+        System.out.println("Your Name?");
+        Name = Keyboard.nextLine();
+
+        greeting(Name);
+        greeting("People");
+
+
+        int x = 10;
+        int y = 3;
+        System.out.print(multiply(x,y));
+
 
 
 
@@ -11,6 +26,17 @@ public class Methods {
     public static void helloWorld(){
 
         System.out.println("Hello World");
+
+    }
+    public static void greeting(String noun){
+        System.out.println("Hello " +noun);
+    }
+    public static int multiply(int number1, int number2){
+        int answer = number1*number2;
+
+        return answer;
+
+
 
     }
 
